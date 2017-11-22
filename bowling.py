43ce1e3d01f953437a666c36bcec0812e5d9da93
Horinka Peter <horinka.peter@gmail.com>
2017-11-22 12:57:30 +0100

    added some blank lines for better code readability throughout the code

--- bowling.py
+++ bowling.py
@@ -2,36 +2,38 @@ def score(game):
     result = 0
     frame = 1
     in_first_half = True
+
     for i in range(len(game)):
         if game[i] == '/':
             result += 10 - last
         else:
             result += get_value(game[i])
-        # if not in_first_half:
-            # frame += 1
         if frame < 10  and get_value(game[i]) == 10:
             if game[i] == '/':
                 result += get_value(game[i+1])
-            elif game[i] == 'X' or game[i] == 'x':
+            elif game[i].lower() == 'x' :
                 result += get_value(game[i+1])
                 if game[i+2] == '/':
                     result += 10 - get_value(game[i+1])
                 else:
                     result += get_value(game[i+2])
+        
         last = get_value(game[i])
+        
         if not in_first_half:
             frame += 1
         if in_first_half == True:
             in_first_half = False
         else:
             in_first_half = True
-        if game[i] == 'X' or game[i] == 'x':
+        if game[i].lower() == 'x':
             in_first_half = True
             frame += 1
     return result
 
 def get_value(char):
     nums = ["1","2","3","4","5","6","7","8","9"]
+    
     if char in nums:
         return int(char)
     elif char.lower() == 'x':
